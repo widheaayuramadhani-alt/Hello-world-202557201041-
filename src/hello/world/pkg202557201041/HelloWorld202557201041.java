ce5812cc49357d3bb3571bebd2bb4fd3547c3ae9
dhea <widheaayuramadhani@gmail.com>
2026-03-19 22:32:24 +0700

    add Hello world

--- src/hello/world/pkg202557201041/HelloWorld202557201041.java
+++ src/hello/world/pkg202557201041/HelloWorld202557201041.java
@@ -15,6 +15,8 @@ public class HelloWorld202557201041 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello world");
     }
+    
     
 }
